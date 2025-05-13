152449da011935aa8b24cf5c79dd54e722756705
Malvoria123 <102215963+Malvoria123@users.noreply.github.com>
2025-05-13 16:39:45 +0700

    Adjusting

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 5,
   message: "Too many requests from this IP. Please try again later.",
 });
 
