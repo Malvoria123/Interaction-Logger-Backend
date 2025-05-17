7f31d1073ae40402277b52619575deb205d6ba9b
Malvoria123 <102215963+Malvoria123@users.noreply.github.com>
2025-05-17 15:52:17 +0700

    Updating to 7 max

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(bodyParser.json());
 // === In-Memory Rate Limiter ===
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
+  max: 7, // limit each IP to small requests per windowMs
   message: {
     status: 429,
     message: "Too many requests from this IP. Please try again later.",
